7ac3a07cb625ca026cf51596a124c6a4d54aa7fe
vrb9999 <vbiradar9999@gmail.com>
2022-05-26 01:08:31 +0530

    Using default parameter

---
+++ Reflection_Parameter/Reflection_Parameter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reflection_Parameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
