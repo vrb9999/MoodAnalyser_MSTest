9b67858c493dc3b889473255f908773373750180
vrb9999 <vbiradar9999@gmail.com>
2022-05-26 01:06:20 +0530

    Using reflection

---
+++ Reflection/Reflection/MoodAnalyserFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Reflection
+{
+    public class MoodAnalyserFactory
+    {
+        public static object CreateeMoodAnalyse(string className, string constructorName)
+        {
+            string pattern = @"." + constructorName + "$";
+            Match result = Regex.Match(className, pattern);
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly executing = Assembly.GetExecutingAssembly();
+                    Type moodAnalyseType = executing.GetType(className);
+                    return Activator.CreateInstance(moodAnalyseType);
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new Custom_MoodAnalyser(Custom_MoodAnalyser.ExceptionType.NO_SUBH_CLASS, "Class Not Found");
+                }
+            }
+            else
+            {
+                throw new Custom_MoodAnalyser(Custom_MoodAnalyser.ExceptionType.NO_SUCH_METHOD, "Constructor is Not Found");
+            }
+        }
+    }
+}
---
+++ Reflection/ReflectionTests/MoodAnalyserTests.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reflection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reflection.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenSadMessage_WhenSad_ShouldReturnSAD()
+        {
+            //Arrange
+            string Expected = "SAD";
+            MoodAnalyser MoodAnalysers = new MoodAnalyser("I am in Sad Mood");
+            //Act
+            string result = MoodAnalysers.AnalyseMood();
+            //Assert
+            Assert.AreEqual(Expected, result);
+        }
+        [TestMethod]
+        public void GivenHappyMessage_WhenAnyMood_ShouldReturnHAPPY()
+        {
+            //Arrange
+            string Expected = "HAPPY";
+            MoodAnalyser MoodAnalysers = new MoodAnalyser("I am in Happy Mood");
+            //Act
+            string result = MoodAnalysers.AnalyseMood();
+            //Assert
+            Assert.AreEqual(Expected, result);
+        }
+        [TestMethod]
+        public void GivenAnyMessage_WhenAnyMood_ShouldReturnHAPPY()
+        {
+            //Arrange
+            string Expected = "HAPPY";
+            MoodAnalyser MoodAnalysers = new MoodAnalyser("I am in Any Mood");
+            //Act
+            string result = MoodAnalysers.AnalyseMood();
+            //Assert
+            Assert.AreEqual(Expected, result);
+        }
+        [TestMethod]
+        [DataRow("null")]
+        public void GivenNULLMessage_WhenANULL_ShouldReturnHAPPY(string message)
+        {
+            //Arrange
+            string Expected = "Happy";
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+            //Act
+            string result = moodAnalyser.AnalyseMood();
+            //Assert
+            Assert.AreEqual(Expected, result);
+        }
+        [TestMethod]
+        [DataRow("")]
+        public void GivenEmptyMessage_WhenEmpty_ShouldThrowException(string message)
+        {
+            try
+            {
+                //Arrange
+                MoodAnalyser MoodAnalysers = new MoodAnalyser(message);
+                //Act
+                string result = MoodAnalysers.AnalyseMood();
+                //Assert
+            }
+            catch (Custom_MoodAnalyser e)
+            {
+                Assert.AreEqual("Mood should not be empty", e.Message);
+            }
+
+        }
+        [TestMethod]
+        public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyserObject()
+        {
+            object expected = new MoodAnalyser();
+            object obj = MoodAnalyserFactory.CreateeMoodAnalyse("Reflection.MoodAnalyser", "MoodAnalyser");
+            expected.Equals(obj);
+        }
+    }
+}
