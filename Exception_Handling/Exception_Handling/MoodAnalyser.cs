3f7e85b8d02fc3b05e9eaa8653fa800402b07983
vrb9999 <vbiradar9999@gmail.com>
2022-05-26 01:01:41 +0530

    Handle Exception if

---
+++ Exception_Handling/Exception_Handling/MoodAnalyser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exception_Handling
+{
+    public class MoodAnalyser
+    {
+        string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public string CheckMood()
+        {
+            try
+            {
+                if (this.message.ToLower().Contains("sad"))
+                {
+                    return "sad";
+                }
+                else
+                {
+                    return "happy";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                return "happy";
+            }
+        }
+    }
+}
---
+++ Exception_Handling/Exception_HandlingTests/MoodAnalyserTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exception_Handling;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exception_Handling.Tests
+{
+    [TestClass()]
+    public class MoodAnalyserTests
+    {
+        MoodAnalyser mac, Mac, macNull;
+        string msg;
+        [TestInitialize]
+        public void Setup()
+        {
+            this.msg = "Im in a Happy Mood";
+            mac = new MoodAnalyser(this.msg);
+            Mac = new MoodAnalyser("im in sad mood");
+            macNull = new MoodAnalyser(null);
+        }
+        [TestMethod]
+        [TestCategory("happy")]
+        public void TestMethodForMessageHappy()
+        {
+            //Arrange
+            string actual, expected = "happy";
+            //Act
+            actual = mac.CheckMood();
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        [TestCategory("sad")]
+        public void TestMethodForMessageSad()
+        {
+            //Arrange
+            string actual, expected = "sad";
+            //Act
+            actual = Mac.CheckMood();
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        [TestCategory("null")]
+        public void TestMethodForMessageNull()
+        {
+            //Arrange
+            string actual, expected = "happy";
+            //Act
+            actual = macNull.CheckMood();
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
